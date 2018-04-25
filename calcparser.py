97688fbb164ae60fa29cfafb69a05d14d0f0b8d7
Ivan Melnik <melnic.ivan@gmail.com>
2018-04-25 19:20:58 +0300

    type parser

--- calcparser.py
+++ calcparser.py
@@ -1,24 +1,31 @@
 from operator import add, sub, mul, truediv, floordiv, mod, pow, lt, le, eq, ne, ge, gt
+from collections import namedtuple
+from math import sin
 
 
-OPERATORS = {
-    '+': add,
-    '-': sub,
-    '*': mul,
-    '/': truediv,
-    '//': floordiv,
-    '%': mod,
-    '^': pow,
-    '<': lt,
-    '<=': le,
-    '==': eq,
-    '!=': ne,
-    '>=': ge,
-    '>': gt,
-    'abs': abs,
-    'pow': pow,
-    'round': round,
-}
+Operator = namedtuple('Operator', 'name function')
+
+
+OPERATORS = [
+    Operator('sin', sin),
+    Operator('abs', abs),
+    Operator('pow', pow),
+    Operator('round', round),
+    Operator('^', pow),
+    Operator('*', mul),
+    Operator('//', floordiv),
+    Operator('/', truediv),
+    Operator('%', mod),
+    Operator('+', add),
+    Operator('-', sub),
+    Operator('<=', le),
+    Operator('<', lt),
+    Operator('==', eq),
+    Operator('!=', ne),
+    Operator('>=', ge),
+    Operator('>', gt),
+]
+
 
 # '(sin(213+34.5) - round(32.3))^2'
 
@@ -66,6 +73,44 @@ def brackets_structure_parser(expr, left='(', right=')'):
     return result
 
 
+def cut_operator(expr, operators=OPERATORS):
+    for operator in operators:
+        if expr.startswith(operator.name):
+            return operator.function, expr.lstrip(operator.name)
+    return None, expr
+
+
+def cut_float(expr, numbers='0123456789.'):
+    number = ''
+    for sym in expr:
+        if sym in numbers:
+            number += sym
+        else:
+            break
+    if number:
+        expr = expr.lstrip(number)
+        number = float(number)
+        return number, expr
+    return None, expr
+
+
+def type_parser(brackets_structure, operators=OPERATORS):
+    result = []
+    for x in brackets_structure:
+        if type(x) is str:
+            while x:
+                res, x = cut_float(x)
+                if res is not None:
+                    result.append(res)
+                res, x = cut_operator(x, operators=operators)
+                if res is not None:
+                    result.append(res)
+        if type(x) is list:
+            result.append(type_parser(x, operators=operators))
+    return result
+
+
 if __name__ == '__main__':
     print(validate_brackets('()   ()()   ((()))'))
-    print(brackets_structure_parser('(sin(213+34.5) - round(32.3))^2'))
+    print(type_parser(brackets_structure_parser('(sin(213+34.5) - round(32.3))^2*0l')))
+    print('done')
